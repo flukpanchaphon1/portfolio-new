98de095abc3dd79e2fd469699805d35b00beeabe
flukpanchaphon1 <flukpanchaphon1@gmail.com>
2022-10-26 23:18:33 +0700

    update recent works

--- src/data/recentWork.js
+++ src/data/recentWork.js
@@ -1,55 +1,55 @@
 export const recentWorks = [
     {
         id: 1,
-        src: './assets/works/previews/polluxdesign.webp',
-        link: 'https://www.polluxdesign.co.th',
-        url: 'www.polluxdesign.co.th',
-        name: 'polluxdesign',
+        src: './assets/works/previews/minnytaxi.webp',
+        link: 'https://minnytaxi.com',
+        url: 'www.minnytaxi.com',
+        name: 'minnytaxi',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
         id: 2,
-        src: './assets/works/previews/sirithornfurniture.webp',
-        link: 'https://www.sirithornfurniture.com',
-        url: 'www.sirithornfurniture.com',
-        name: 'sirithornfurniture',
+        src: './assets/works/previews/ขออย.webp',
+        link: 'https://www.ขออย.com',
+        url: 'www.ขออย.com',
+        name: 'ขออย',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
         id: 3,
-        src: './assets/works/previews/rush.webp',
-        link: 'https://rush.in.th',
-        url: 'www.rush.in.th',
-        name: 'rush',
+        src: './assets/works/previews/greenenviengineering.webp',
+        link: 'https://greenenviengineering.com',
+        url: 'www.greenenviengineering.com',
+        name: 'greenenviengineering',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
         id: 4,
-        src: './assets/works/previews/carryhealthofficial.webp',
-        link: 'https://carryhealthofficial.com',
-        url: 'www.carryhealthofficial.com',
-        name: 'carryhealthofficial',
+        src: './assets/works/previews/spkeducation.webp',
+        link: 'https://spkeducation.co.th',
+        url: 'www.spkeducation.co.th',
+        name: 'spkeducation',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
         id: 5,
-        src: './assets/works/previews/picnikmarket.webp',
-        link: 'https://picnikmarket.com',
-        url: 'www.picnikmarket.com',
-        name: 'picnikmarket',
+        src: './assets/works/previews/taxipattaya.webp',
+        link: 'https://taxipattaya.info',
+        url: 'www.taxipattaya.info',
+        name: 'taxipattaya',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
         id: 6,
-        src: './assets/works/previews/ngsolarplus.webp',
-        link: 'https://ngsolarplus.com',
-        url: 'www.ngsolarplus.com',
-        name: 'ngsolarplus',
+        src: './assets/works/previews/polluxdesign.webp',
+        link: 'https://polluxdesign.co.th',
+        url: 'www.polluxdesign.co.th',
+        name: 'polluxdesign',
         cate: 'WordPress',
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
@@ -64,6 +64,60 @@ export const recentWorks = [
     },
     {
         id: 8,
+        src: './assets/works/previews/ngsolarplus.webp',
+        link: 'https://www.ngsolarplus.com',
+        url: 'www.ngsolarplus.com',
+        name: 'ngsolarplus',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 9,
+        src: './assets/works/previews/mini-social-app.webp',
+        link: '#',
+        url: '#',
+        name: 'Mini Social App',
+        cate: 'MyProjects',
+        tag: ['ReactJS', 'CSS', 'NodeJS', 'MongoDB', 'ExpressJS']
+    },
+    {
+        id: 10,
+        src: './assets/works/previews/sirithornfurniture.webp',
+        link: 'https://www.sirithornfurniture.com',
+        url: 'www.sirithornfurniture.com',
+        name: 'sirithornfurniture',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 11,
+        src: './assets/works/previews/rush.webp',
+        link: 'https://www.rush.in.th',
+        url: 'www.rush.in.th',
+        name: 'rush',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 12,
+        src: './assets/works/previews/picnikmarket.webp',
+        link: 'https://www.picnikmarket.com',
+        url: 'www.picnikmarket.com',
+        name: 'picnikmarket',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 13,
+        src: './assets/works/previews/arnexttech.webp',
+        link: 'https://www.arnexttech.net',
+        url: 'www.arnexttech.net',
+        name: 'arnexttech',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 14,
         src: './assets/works/previews/mashangthai.webp',
         link: 'https://www.mashangthai.com',
         url: 'www.mashangthai.com',
@@ -72,13 +126,31 @@ export const recentWorks = [
         tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
     {
-        id: 8,
-        src: './assets/works/previews/mini-social-app.webp',
-        link: '#',
-        url: '#',
-        name: 'Mini Social App',
-        cate: 'MyProjects',
-        tag: ['ReactJS', 'CSS', 'NodeJS', 'MongoDB', 'ExpressJS']
+        id: 15,
+        src: './assets/works/previews/carryhealthofficial.webp',
+        link: 'https://www.carryhealthofficial.com',
+        url: 'www.carryhealthofficial.com',
+        name: 'carryhealthofficial',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 16,
+        src: './assets/works/previews/Minori.webp',
+        link: 'https://www.Minori.in.th',
+        url: 'www.Minori.in.th',
+        name: 'Minori',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
+    },
+    {
+        id: 17,
+        src: './assets/works/previews/mldcosmetics.webp',
+        link: 'https://www.mldcosmetics.com',
+        url: 'www.mldcosmetics.com',
+        name: 'mldcosmetics',
+        cate: 'WordPress',
+        tag: ['CMS', 'WORDPRESS', 'PHP', 'CSS', 'HTML', 'JavaScript']
     },
 ]
 
